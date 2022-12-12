e3db664b06556b5480cb859d1f71fd6bc1febb4d
sameersinha3 <sameer_sinha+cs1300@brown.edu>
2022-12-12 12:22:35 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+import React from "react";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 import './App.css';
-import BerkshireHathawayRedesign from './components/BerkshireHathawayRedesign';
-import GOT from './components/GOT'
-import Header from './components/Header.js'
-import JerseyStore from './components/JerseyStore';
-import Navigation from './components/Navigation.js'
-import PortfolioItem from './components/PortfolioItem.js'
-import Projects from './components/Projects';
+import ProjectDescriptions from "./components/ProjectDescriptions";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <Navigation></Navigation>
-      <Header></Header>
-      <Projects></Projects>
-
-    </div>
+    <main className="body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <ProjectDescriptions />
+      <Skills />
+      
+    </main>
   );
 }
-
-export default App;
